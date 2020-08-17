e7c3fe6fbf66d189d769239536511ced2a0d3668
nickmccurdy <nickmccurdy@users.noreply.github.com>
2020-08-17 11:23:19 +0000

    deploy: fcb759bf841ce315b42c03be8d2602d9a747ad12

---
+++ precache-manifest.fc71103a09e4914335926a87244f7828.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fe84530ec2db64d8dab8bd4476fa7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef30a4000697a8117d1e",
+    "url": "./static/css/2.899392aa.chunk.css"
+  },
+  {
+    "revision": "10c057869ba2859ea7c5",
+    "url": "./static/css/main.7e1d6ddd.chunk.css"
+  },
+  {
+    "revision": "ef30a4000697a8117d1e",
+    "url": "./static/js/2.4b41b0c3.chunk.js"
+  },
+  {
+    "revision": "e55895c631c916fedaacae97755e295d",
+    "url": "./static/js/2.4b41b0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c057869ba2859ea7c5",
+    "url": "./static/js/main.ddf82152.chunk.js"
+  },
+  {
+    "revision": "eb5b5aad2062bcbdeac4",
+    "url": "./static/js/runtime-main.a37ffe28.js"
+  },
+  {
+    "revision": "a39fead67118632a04c96b35bc469b4f",
+    "url": "./static/media/fredoka-one-latin-400.a39fead6.woff"
+  },
+  {
+    "revision": "e12e0aa363a60ce6f2140ba7565cadf6",
+    "url": "./static/media/fredoka-one-latin-400.e12e0aa3.woff2"
+  }
+]);
