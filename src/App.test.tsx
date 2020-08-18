48b1158541e79ab5dcc9ca3d863c786459b3c4af
Nick McCurdy <nick@nickmccurdy.com>
2020-08-18 03:32:58 -0400

    Add initial test for focus management

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import App from "./App"
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+test("focuses new order when adding", () => {
+  render(<App />)
+  userEvent.click(screen.getByText("Add"))
+  expect(document.querySelector("table input")).toHaveFocus()
 })
