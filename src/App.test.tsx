7b114e6d390c6694ba2ca19ca7c14338e76a9a39
Nick McCurdy <nick@nickmccurdy.com>
2020-08-20 09:41:39 -0400

    Add waitFor to squash Formik's act warnings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import App from "./App"
 
-test("focuses new order when adding", () => {
+test("focuses new order when adding", async () => {
   render(<App />)
   userEvent.click(screen.getByText("Add"))
-  expect(document.querySelector("table input")).toHaveFocus()
+  await waitFor(() =>
+    expect(document.querySelector("table input")).toHaveFocus()
+  )
 })
