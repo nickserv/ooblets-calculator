1f23830e36b82ecf13d8ae6b6624eab4a026ea5a
Nick McCurdy <nick@nickmccurdy.com>
2022-07-25 01:39:13 -0400

    Upgrade dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,11 @@
-import { render, screen, waitFor, within } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import App from "./App"
 
 test("focuses new order when adding", async () => {
   render(<App />)
-  userEvent.click(screen.getByText("Add"))
-  await waitFor(() => {
-    expect(
-      within(screen.getByRole("table")).getAllByRole("spinbutton")[0],
-    ).toHaveFocus()
-  })
+  await userEvent.click(screen.getByText("Add"))
+  expect(
+    within(screen.getByRole("table")).getAllByRole("spinbutton")[0],
+  ).toHaveFocus()
 })
