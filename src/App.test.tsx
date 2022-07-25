de5be11be49fd06bbb5721b683f75a6f8669dfe9
Nick McCurdy <nick@nickmccurdy.com>
2022-07-25 01:15:57 -0400

    Wait for element to appear in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen, within } from "@testing-library/react"
+import { render, screen, waitFor, within } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import App from "./App"
 
 test("focuses new order when adding", async () => {
   render(<App />)
   userEvent.click(screen.getByText("Add"))
-  expect(
-    within(screen.getByRole("table")).getAllByRole("spinbutton")[0],
-  ).toHaveFocus()
+  await waitFor(() => {
+    expect(
+      within(screen.getByRole("table")).getAllByRole("spinbutton")[0],
+    ).toHaveFocus()
+  })
 })
