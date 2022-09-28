045155514b6f176e19280a4d5a20c5b99a55970c
Nick McCurdy <nick@nickmccurdy.com>
2022-09-27 22:32:39 -0400

    Remove vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from "vitest/config"
-import checker from "vite-plugin-checker"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: "eslint --ignore-path .gitignore .",
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     open: true,
   },
