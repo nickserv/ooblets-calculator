0ff6652f36b66facac267d58cc1f93dff584451a
Nick McCurdy <nick@nickmccurdy.com>
2024-02-02 02:03:14 -0500

    Add payment data

--- src/items.ts
+++ src/items.ts
@@ -1,7 +1,7 @@
 // Re-export items.json with better types
 import inferredItems from "./items.json"
 
-type Item = { name: string } & (
+type Item = { name: string; payment: number } & (
   | {}
   | { cost: number; days: number }
   | { from: string[] }
